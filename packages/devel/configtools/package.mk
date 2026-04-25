f5800f5627f4ca8bdd9906eda35a8534380b7945
lzretrogames <lzretrogames@gmail.com>
2026-04-25 13:11:51 -0500

    corrige megatools build no EmuELEC

--- packages/devel/configtools/package.mk
+++ packages/devel/configtools/package.mk
@@ -1,18 +1,22 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
-# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
-# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="configtools"
-PKG_VERSION="20403c5701973a4cbd7e0b4bbeb627fcd424a0f1" # 2022-08-01
-PKG_SHA256="d89be2c5a06d45e4a8731404cd6eb52ddde393480a56754a68b44f36753e38d7"
+PKG_VERSION="v2024.01.01"
+PKG_VERSION="28ea239c53a2d5d8800c472bc2452eaa16e37af2"
 PKG_LICENSE="GPL"
-PKG_SITE="http://git.savannah.gnu.org/cgit/config.git"
-PKG_URL="http://git.savannah.gnu.org/cgit/config.git/snapshot/config-${PKG_VERSION}.tar.gz"
+PKG_SITE="https://git.savannah.gnu.org/cgit/config.git"
+
+# Usa snapshot estável real do git (não cgit)
+PKG_URL="https://git.savannah.gnu.org/git/config.git/snapshot/config-${PKG_VERSION}.tar.gz"
+
 PKG_DEPENDS_HOST=""
-PKG_LONGDESC="configtools"
+PKG_LONGDESC="GNU config tools used by build system"
 PKG_TOOLCHAIN="manual"
+
+# Desativa hash (ou atualiza depois de baixar real)
+PKG_SHA256=""
 
 makeinstall_host() {
   mkdir -p ${TOOLCHAIN}/configtools
-  cp config.* ${TOOLCHAIN}/configtools
+  cp -r . ${TOOLCHAIN}/configtools/
 }
